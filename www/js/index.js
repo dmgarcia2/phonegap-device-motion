67dd998e5b409c0ee30646b41e8e406619f6f0cb
David Martín García <david@iasmobile.es>
2015-01-13 00:04:16 +0100

    Device motion plugin add

--- www/js/index.js
+++ www/js/index.js
@@ -10,6 +10,21 @@ application.app = {
     onDeviceReady: function() {
         StatusBar.hide();
         FastClick.attach(document.body);
+    },
+
+    getCurrentAcceleration: function() {
+        navigator.accelerometer.getCurrentAcceleration(application.app.showAccel, application.app.onError);
+    },
+
+    showAccel: function(acceleration) {
+        alert('Acceleration X: ' + acceleration.x + '\n' +
+                'Acceleration Y: ' + acceleration.y + '\n' +
+                'Acceleration Z: ' + acceleration.z + '\n' +
+                'Timestamp: ' + acceleration.timestamp + '\n');
+    },
+
+    onError: function() {
+        alert("Error recuperando datos de acelerómetro.");
     }
 
 };
